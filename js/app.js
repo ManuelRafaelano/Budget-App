0863feb76b08f0e5a0b7cecdfcfd75454dd4d633
Manuel Alexander Vasquez Rafaelano <manuelrafaelano@gmail.com>
2019-07-15 19:28:38 -0600

    create variables

--- js/app.js
+++ js/app.js
@@ -16,3 +16,11 @@ class UI {
     this.itemID = 0;
   }
 }
+function eventListeners() {
+  const budgetForm = document.getElementById("budget-form");
+  const expenseForm = document.getElementById("expense-form");
+  const expenseList = document.getElementById("expense-list");
+}
+document.addEventListener("DOMContentLoaded", function() {
+  eventListeners();
+});
